acd78ab70921e39d21d8d8abc5ebdc91c2e907a6
yoja <zyj@MacBook-Air-32.local>
2025-10-13 10:34:54 +0200

    Correction du script

--- exercicedata/ann/Exercices:comptes.sh
+++ exercicedata/ann/Exercices:comptes.sh
@@ -1,4 +1,4 @@
-#！/bin/bash
-echo "2016: $(cat 2016*.ann | grep Location | wc -l)" > Locations_comptes.txt
-echo "2017: $(cat 2017*.ann | grep Location | wc -l)" >> Locations_comptes.txt
-echo "2018: $(cat 2018*.ann | grep Location | wc -l)" >> Locations_comptes.txt
+#!/bin/bash
+echo "2016: $(cat 2016*.ann | grep Location | wc -l)"
+echo "2017: $(cat 2017*.ann | grep Location | wc -l)"
+echo "2018: $(cat 2018*.ann | grep Location | wc -l)"
