a51c0da26afdd98124ffbf7d493220a14631ba9f
yoja <zyj@wifi-16-14-145.wifi.univ-paris3.fr>
2025-10-15 11:03:07 +0200

    correction

--- Exercices/compte_lieux.sh
+++ Exercices/compte_lieux.sh
@@ -2,7 +2,8 @@
 annee=$1
 mois=$2
 nombre=$3
-fichier=$(ls ./${annee}_${mois}*.ann)
+datadir=$4
+fichier=$(ls ${datadir}/${annee}_${mois}*.ann)
 
 echo "classement des lieux les plus cités: "
 cat $fichier | grep Location | cut -f3 | sort | uniq -c | sort -nr | head -n $nombre
